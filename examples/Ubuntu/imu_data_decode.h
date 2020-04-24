fd82446cf52114f858f0a197d17c615070d9e7ce
bai-bit <1637274023@qq.com>
2020-04-24 14:03:35 +0800

    add struct data

--- examples/Ubuntu/imu_data_decode.h
+++ examples/Ubuntu/imu_data_decode.h
@@ -5,16 +5,9 @@
 #include <stdbool.h>
 #define MAX_LENGTH 16
 
-#define BIT_VALID_ID	(0)
-#define BIT_VALID_ACC	(1)
-#define BIT_VALID_GYR	(2)
-#define BIT_VALID_MAG	(3)
-#define BIT_VALID_EUL	(4)
-#define BIT_VALID_QUAT	(5)
 
 extern int frame_count;
 
-<<<<<<< HEAD
 #define BIT_VALID_ID   (0)
 #define BIT_VALID_ACC  (1)
 #define BIT_VALID_GYR  (2)
@@ -28,19 +21,11 @@ typedef struct receive_imusol_packet_t {
 	uint8_t bitmap;
 	uint8_t	resereve[10];
 
-=======
-
-typedef struct
-{
-	uint8_t id;
-	uint8_t bitmap;		/* each bit indicate data valid or not: bit0:acc  bit1:gyr,  bit2:mag */
->>>>>>> fd06b4be97443ee993410c66ccfe70d59c711947
 	float acc[3];
 	float gyr[3];
 	float mag[3];
 	float eul[3];
 	float quat[4];
-<<<<<<< HEAD
 
 }receive_imusol_packet_t;
 
@@ -53,10 +38,6 @@ typedef struct receive_gwsol_packet_t {
 	receive_imusol_packet_t receive_imusol[MAX_LENGTH];
 }receive_gwsol_packet_t;
 	
-=======
-}imt_data_t;
-
->>>>>>> fd06b4be97443ee993410c66ccfe70d59c711947
 typedef enum 
 {
     kItemID =                   0x90,   /* user programed ID   */
@@ -72,20 +53,8 @@ typedef enum
 }ItemID_t;
 
 int imu_data_decode_init(void);
-<<<<<<< HEAD
 int get_imu_data(receive_imusol_packet_t *data);
 int get_gw_data(receive_gwsol_packet_t *data);
-=======
-//int get_raw_acc(float* a);
-//int get_frame_count(void);
-//int get_raw_gyr(float* g);
-//int get_raw_mag(float* m);
-//int get_id(uint8_t *user_id);
-//int get_eul(float* e);
-//int get_quat(float* q);
-
-int get_imu_data(imt_data_t *data);
->>>>>>> fd06b4be97443ee993410c66ccfe70d59c711947
 
 #endif
 
