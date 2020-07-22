fdb2c5cc2a6ca58184437259b09be324e4f762a9
bai-bit <1637274023@qq.com>
2020-07-22 14:20:02 +0800

    modified serial_imu.cpp ,add imu_tool publisher rviz

--- examples/ROS/serial_imu/src/serial_imu.cpp
+++ examples/ROS/serial_imu/src/serial_imu.cpp
@@ -8,8 +8,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sensor_msgs/Imu.h>
+#include <signal.h>
 
-#ifdef __cplusplus
+#ifdef __cplusplus 
 extern "C"{
 #endif
 #include <stdint.h>
@@ -31,29 +33,14 @@ static int frame_rate;
 
 static uint8_t buf[2048];
 
-int time_out(int second)
+void timer(int sig)
 {
-	struct timeval time_value;
-	time_value.tv_sec = second;
-	time_value.tv_usec = 0;
-
-	return select(0,NULL,NULL,NULL,&time_value);
-}
-
-int ret_frame_count(void)
-{
-	frame_count = 0;
-	int i = time_out(1);
-
-	return frame_count;
-}
-
-void *pthread_frame_rate(void *arg)
-{
-	while(1)
-		frame_rate = ret_frame_count();
-
-	pthread_exit(NULL);
+	if(SIGALRM == sig)
+	{
+		frame_rate = frame_count;
+		frame_count = 0;
+		alarm(1);
+	}
 }
 
 int main(int argc, char** argv)
@@ -61,6 +48,8 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "serial_imu");
     //创建句柄（虽然后面没用到这个句柄，但如果不创建，运行时进程会出错）
     ros::NodeHandle n;
+
+	ros::Publisher IMU_pub = n.advertise<sensor_msgs::Imu>("IMU_data", 20);
 
     //创建一个serial类
     serial::Serial sp;
@@ -74,7 +63,8 @@ int main(int argc, char** argv)
     sp.setTimeout(to);
 	
 	imu_data_decode_init();
- 
+ 	signal(SIGALRM,timer);
+
     try
     {
         //打开串口
@@ -96,8 +86,7 @@ int main(int argc, char** argv)
         return -1;
     }
 	
-	pthread_t ph_rate;
-	int ret_pth = pthread_create(&ph_rate,NULL,pthread_frame_rate,NULL);
+	alarm(1);
     ros::Rate loop_rate(500);
 
     while(ros::ok())
@@ -113,11 +102,21 @@ int main(int argc, char** argv)
 			{
 				for(int i = 0; i < n; i++)
 					packet_decode(buffer[i]);
+
+				sensor_msgs::Imu imu_data;
+				imu_data.header.stamp = ros::Time::now();
+				imu_data.header.frame_id = "base_link";
+
 				puts("\033c");
-				if(receive_gwsol.tag != KItemGWSOL)
+ 				if(receive_gwsol.tag != KItemGWSOL)
 				{
 					dump_data_packet(&receive_imusol);
-					puts("Pleaes enter ctrl + 'c' and ctrl + 'z' to quit....");
+					imu_data.orientation.x = receive_imusol.quat[2];
+					imu_data.orientation.y = -receive_imusol.quat[1];
+					imu_data.orientation.z = -receive_imusol.quat[0];
+					imu_data.orientation.w = receive_imusol.quat[3];
+					IMU_pub.publish(imu_data);
+					puts("Pleaes enter ctrl + 'c' to quit....");
 				}
 				else
 				{
@@ -127,7 +126,7 @@ int main(int argc, char** argv)
 						dump_data_packet(&receive_gwsol.receive_imusol[i]);
 						puts("");
 					}
-					puts("Please enter ctrl + 'c' and ctrl + 'z' to quit...");
+					puts("Please enter ctrl + 'c' to quit...");
 				}
 			}
         }
@@ -136,7 +135,6 @@ int main(int argc, char** argv)
     
 	//关闭串口
 	sp.close();
-	pthread_join(ph_rate, NULL);
  
 	return 0;
 }
@@ -149,7 +147,7 @@ void dump_data_packet(receive_imusol_packet_t *data)
 	}
 	if(bitmap & BIT_VALID_TIMES)
 	{
-		printf("    Run times:%-8d\n",data->times % 1000);
+		printf("    Run times:%d days %d:%d:%d:%d\n",data->times / 86400000, data->times / 3600000 % 24, data->times / 60000 % 60, data->times / 1000 % 60, data->times % 1000);
 	}
 	printf("  Frame Rate:  %4dHz\r\n",frame_rate);
 	if(bitmap & BIT_VALID_ACC)
