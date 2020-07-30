4b9682f74375e953d3341452c41317c31f72219f
bai-bit <55228766+bai-bit@users.noreply.github.com>
2020-07-30 19:10:04 +0800

    Update subscriber.cpp

--- examples/ROS/serial_imu_ws/src/serial_imu/src/subscriber.cpp
+++ examples/ROS/serial_imu_ws/src/serial_imu/src/subscriber.cpp
@@ -14,8 +14,8 @@ int main(int argc,char **argv)
 	ros::init(argc,argv,"imu_subscriber");
 
 	ros::NodeHandle n;
-#if ROSTOPIC_ECHO
 	
+#if ROSTOPIC_ECHO
 	execlp("rostopic", "rostopic", "echo", "/IMU_data", NULL);
 #endif
 	ros::Subscriber imu_sub = n.subscribe("/IMU_data", 10,imu_callback);
@@ -27,7 +27,7 @@ void imu_callback(const sensor_msgs::Imu msg)
 {
 	printf("\033c");
 
-    printf("Quat(W X Y Z):  %8.3f	%8.3f	%8.3f	%8.3f\r\n", msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z);
+	printf("Quat(W X Y Z):  %8.3f	%8.3f	%8.3f	%8.3f\r\n", msg.orientation.w, msg.orientation.x, msg.orientation.y, msg.orientation.z);
 
 	printf("   Gyr(deg/s):			%8.3f	%8.3f	%8.3f\r\n", msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z);
 
