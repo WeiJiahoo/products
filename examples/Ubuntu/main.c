d7f4f4afddf6570ad59ab5d7387b94675d3e5641
bai-bit <1637274023@qq.com>
2020-07-30 19:03:35 +0800

    update Ubuntu/main.c

--- examples/Ubuntu/main.c
+++ examples/Ubuntu/main.c
@@ -29,25 +29,25 @@ void dump_data_packet(receive_imusol_packet_t *data);
 	
 int open_port(char *port_device)
 {
-   struct termios options;
+	struct termios options;
 
 	int fd = open(port_device, O_RDWR | O_NOCTTY);
 	
 	tcgetattr(fd, &options);
 
 	if (fd == -1)
-    {
-        perror("open_port: Unable to open SerialPort");
+	{
+		perror("open_port: Unable to open SerialPort");
 		puts("Please check the usb port name!!!");
 		puts("such as \" sudo ./main ttyUSB0 \"");
 		exit(0);
 	}
 
-    if(fcntl(fd, F_SETFL, 0)<0)
+	if(fcntl(fd, F_SETFL, 0)<0)
 	{
 		printf("fcntl failed\n");
 	}
-    else
+	else
 	{
 		fcntl(fd, F_SETFL, 0);
 	}
@@ -69,9 +69,9 @@ int open_port(char *port_device)
 	options.c_oflag = 0;
 	options.c_lflag = 0;
 	options.c_cc[VTIME] = 0;
-    options.c_cc[VMIN] = 0;
+	options.c_cc[VMIN] = 0;
 	tcflush(fd,TCIFLUSH);
-    tcsetattr(fd, TCSANOW, &options);
+	tcsetattr(fd, TCSANOW, &options);
 	return (fd);
 }
 
@@ -89,7 +89,7 @@ void timer(int sig)
 int main(int argc, const char *argv[])
 {
 	int fd = 0;
-    char dir_usb_dev[64] = "/dev/";
+	char dir_usb_dev[64] = "/dev/";
 
 	int i;
 	ssize_t n = 0;
@@ -98,7 +98,7 @@ int main(int argc, const char *argv[])
 	if(argc >1)
 	{
 		strcat(dir_usb_dev, argv[1]);
-	  	fd = open_port(dir_usb_dev);
+		fd = open_port(dir_usb_dev);
 	}
 	else
 	{
@@ -125,10 +125,9 @@ int main(int argc, const char *argv[])
 			{
 			//	 printf imu data packet 
 				dump_data_packet(&receive_imusol);
-				
+
 				puts("Please enter ctrl + 'c' to quit...");
 			}
- 
 			else
 			{
 			//	 printf gw data packet 
@@ -145,26 +144,32 @@ int main(int argc, const char *argv[])
 	}	
 	sleep(1);
 
-    close(fd);
+	close(fd);
 }
 
 void dump_data_packet(receive_imusol_packet_t *data)
 {
 	if(bitmap & BIT_VALID_ID)
 		printf("    Device ID:  %-8d\n",  data->id);
+
 	if(bitmap & BIT_VALID_TIMES)
 		printf("Run times(ms):  %d days  %d:%d:%d:%d \n",data->times / 86400000, data->times / 3600000 % 24, data->times / 60000 % 60, data->times / 1000 % 60, data->times % 1000);
+
 	printf("   Frame Rate: %4dHz\n", frame_rate);
+
 	if(bitmap & BIT_VALID_ACC)
 		printf("       Acc(G):	%8.3f %8.3f %8.3f\r\n",  data->acc[0],  data->acc[1],  data->acc[2]);
+
 	if(bitmap & BIT_VALID_GYR)
 		printf("   gyr(deg/s):	%8.2f %8.2f %8.2f\r\n",  data->gyr[0],  data->gyr[1],  data->gyr[2]);
+
 	if(bitmap & BIT_VALID_MAG)
 		printf("      mag(uT):	%8.2f %8.2f %8.2f\r\n",  data->mag[0],  data->mag[1],  data->mag[2]);
+
 	if(bitmap & BIT_VALID_EUL)
 		printf("   eul(R P Y):  %8.2f %8.2f %8.2f\r\n",  data->eul[0],  data->eul[1],  data->eul[2]);
+
 	if(bitmap & BIT_VALID_QUAT)
 		printf("quat(W X Y Z):  %8.3f %8.3f %8.3f %8.3f\r\n",  data->quat[0],  data->quat[1],  data->quat[2],  data->quat[3]);
-
 }
 			
